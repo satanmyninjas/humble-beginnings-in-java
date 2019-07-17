e6e821f8eda64ab02a685cb0cfeca768331056b5
your favorite edgelord™ <35822767+satanmyninjas@users.noreply.github.com>
2019-07-17 11:01:15 -0400

    Create IntroArrays.java

---
+++ IntroArrays.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class IntroArrays {
+    public static void main (String[] args){
+    // Cannot mix your data types in Java. You must stay consistent.
+    
+    // Declare them like you would for a variable.
+    String[] fantasyNames;
+    int[] healthPoints;
+    
+    // Like in Python, you can access elements of an array
+    // via indexing; start from zero.
+    
+    String[] reindeernames = {"Dasher", "Cupid", "Prancer",
+                              "Praxis", "Lucia", "Rudolph"};
+    
+    // Muti-dimensional array. Denoted via [][]
+    int [][] houseLocation = new int [10][7];
+    // There are now 70 elements in this array; 10 x 7
+    
+    // You can then just "fill in the grid"
+    }
+}
