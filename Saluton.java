5621dc3cb124beaccf37bb2e2439bd7494eb7a0d
your favorite edgelord™ <35822767+satanmyninjas@users.noreply.github.com>
2019-07-17 10:48:15 -0400

    Create Saluton.java

---
+++ Saluton.java
@@ -0,0 +1,8 @@
+class Saluton {
+    public static void main (String[] arguments) {
+        String greeting = "Hello world!";
+            System.out.println (greeting);  
+        // printing some stuff out for a beginner project
+        // 7/9/2019 FEMME 9
+    }       
+}
